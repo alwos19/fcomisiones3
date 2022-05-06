b8bebcb06494bded280f1e236416685decbd9d37
alwos19 <alwos19>
2022-05-06 10:49:33 -0500

    search comisiones

--- src/app/solicitudes/components/solicitudes-table/solicitudes-table.component.ts
+++ src/app/solicitudes/components/solicitudes-table/solicitudes-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comision } from '../../interfaces/comisiones/comision';
+import { ComisionService} from '../../services/comisiones/comision.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-solicitudes-table',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudesTableComponent implements OnInit {
 
-  constructor() { }
+  // comisiones$: Observable<Comision[]>;
+  // total$: Observable<number>
+
+  constructor(
+
+    public comisionService: ComisionService,
+    // public searchSolicitudesService: SearchSolicitudesService,
+    public router: Router
+  ) {
+
+
+  }
 
   ngOnInit(): void {
+    // this.comisiones = this.comisionService;
   }
 
 }
---
+++ src/app/solicitudes/interfaces/comisiones/tipo-comision.ts
@@ -0,0 +1,6 @@
+export interface TipoComision {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  comisiones: [];
+}
